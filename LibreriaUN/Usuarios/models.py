932c8d802a3dff1bbf6e9aefdd7f2c2e1c05543a
unknown <dballesterosb@unal.edu.co>
2022-11-25 23:19:41 -0500

    estrellas libro

--- LibreriaUN/Usuarios/models.py
+++ LibreriaUN/Usuarios/models.py
@@ -1,7 +1,26 @@
 from django.db import models
 from django.contrib.auth.models import User
+#from django.contrib.auth.models import AbstractUser
+#from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+
+
+"""class User(AbstractUser):
+  class Types(models.TextChoices):
+    Cliente = "Cliente"
+    Empleado = "Empleado"
+  type = models.CharField(_("Type"), max_length= 20, choices=Types.choices, default=Types.Cliente)
+
+
+class Empleado(User):
+  class Meta:
+    proxy = True
+
+class Cliente(User):
+  class Meta:
+    proxy = True
+"""
 
 class Usuario(models.Model):
   user = models.OneToOneField(User, on_delete=models.CASCADE)
