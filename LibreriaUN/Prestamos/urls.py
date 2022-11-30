54e6af42b3d7fb20f6b64bb49f01bf9bcded5db6
SebastianOG509 <seortizg@unal.edu.co>
2022-11-29 23:02:03 -0500

    Se agregaron paginas Sobre Nosotros y Contacto

--- LibreriaUN/Prestamos/urls.py
+++ LibreriaUN/Prestamos/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('prestamo', views.Prestamo, name='Prestamo'),
+    path('', views.Prestar, name='Prestamo'),
 ]
--- LibreriaUN/Prestamos/views.py
+++ LibreriaUN/Prestamos/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from .models import Prestamo, Libro, Cliente, Empleado
+
 
 # Create your views here.
-def Prestamo(request):
+def Prestar(request):
 
-    return render(request, "Prestamo/libros.html")
+    return render(request, "Prestamos/prestamo.html")
+
+def Hacer_prestamo(request, cliente_id, empleado_id, libro_id):
+    libro = Libro.objects.get(pk=libro_id)
+    cliente = Cliente.objects.get(pk=libro_id)
+    Prestamo = Empleado.objects.get(pk=libro_id)
+    
+    return render(request, "Prestamos/prestamo.html")
+
